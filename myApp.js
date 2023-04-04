713a5ca07dc1372503935f2d9b55e9bc5418f73a
LianSheng197 <ci.i.cxcvii@gmail.com>
2023-04-04 22:07:35 +0800

    helmet.contentSecurityPolicy

--- myApp.js
+++ myApp.js
@@ -17,7 +17,12 @@ app.use(helmet.hsts({
 }));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "trusted-cdn.com"]
+  },
+}))
 
 
 
